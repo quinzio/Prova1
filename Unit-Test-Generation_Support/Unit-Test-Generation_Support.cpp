57f799b609f36440a35b9683fe5ec08f655641ae
Davide Munari <quinzio2@yahoo.it>
2020-01-21 17:01:26 +0100

    Work in progress

--- Unit-Test-Generation_Support/Unit-Test-Generation_Support.cpp
+++ Unit-Test-Generation_Support/Unit-Test-Generation_Support.cpp
@@ -74,7 +74,7 @@ int main()
     std::cout << sm[1] << "\n";
     std::cout << sm[2] << "\n";
 
-    std::ifstream infile("Text.txt");
+    std::ifstream infile("Text2.txt");
     std::string str;
     std::regex eCatchGlobals    (R"(([^\w<]*)([\w<]+).*)");
     std::smatch smCatchGlobals;
@@ -164,6 +164,8 @@ Variable visit(Node *node)
     std::smatch smImplicitCastExpr;
     std::regex eBinaryOperator(R"###([^\w<]*[\w<]+\s0x[\da-f]{6,7}\s<[^>]*>\s'([^']+)'\s'([^']+)')###");
     std::smatch smBinaryOperator;
+    std::regex eUnaryOperator(R"###([^\w<]*[\w<]+\s0x[\da-f]{6,7}\s<[^>]*>\s'([^']+)'\s(postfix|prefix)\s'([^']+)')###");
+    std::smatch smUnaryOperator;
     std::regex eDeclStmt(R"###([^\w<]*[\w<]+\s0x[\da-f]{6,7}\s<[^>]*>)###");
     std::smatch smDeclStmt;
     /*
@@ -185,13 +187,15 @@ Variable visit(Node *node)
         TypedefDecl
         VarDecl
     */
-    Variable ret;
     if (node->astType.compare("IntegerLiteral") == 0) {
         unsigned long long integral;
+        Variable ret;
         std::regex_search(node->text, smIntegralType, eIntegralType);
         integral = std::stoi(smIntegralType[2]);
         ret.value = cast(smIntegralType[1], integral);
+        return ret;
     }
+
     if (node->astType.compare("VarDecl") == 0) {
         std::regex_search(node->text, smVarDecl, eVarDecl);
         Variable temp;
@@ -199,52 +203,143 @@ Variable visit(Node *node)
         temp.type = smVarDecl[2];
         temp.value = 0;
         vVariable.push_back(temp);
-        ret.value = 0;
+        return temp;
     }
+
     if (node->astType.compare("DeclRefExpr") == 0) {
+        Variable ret;
         std::regex_search(node->text, smDeclRefExpr, eDeclRefExpr);
         std::string varName = smDeclRefExpr[2];
         for (auto it = vVariable.begin(); it != vVariable.end(); it++) {
             if (it->name.compare(varName) == 0) {
-                return *it;
+                ret = *it;
+                return ret;
             }
         }
     }
+
     if (node->astType.compare("ImplicitCastExpr") == 0) {
+        Variable ret;
         std::regex_search(node->text, smImplicitCastExpr, eImplicitCastExpr);
-        std::string castTo = smDeclRefExpr[1];
+        std::string castTo = smImplicitCastExpr[1];
         ret = visit(node->child);
         ret.value = cast(castTo, ret.value);
-    }
-    if (node->astType.compare("BinaryOperator") == 0) {
-        std::regex_search(node->text, smBinaryOperator, eBinaryOperator);
-        std::string castTo = smDeclRefExpr[1];
-        std::string boperator = smDeclRefExpr[2];
-        Variable opa, opb;
-        opa = visit(node->child);
-        opb = visit(node->child->nextSib);
-        if (boperator.compare("<")) {
-            ret.value = opa.value < opb.value;
-        }
+        return ret;
     }
 
+    if (node->astType.compare("UnaryOperator") == 0) {
+        Variable opa, ret;
+        std::regex_search(node->text, smUnaryOperator, eUnaryOperator);
+        std::string castTo = smUnaryOperator[1];
+        std::string fix = smUnaryOperator[2];
+        std::string uoperator = smUnaryOperator[3];
+        opa = visit(node->child);
+        Variable* mod = NULL;
+        for (auto it = vVariable.begin(); it != vVariable.end(); it++) {
+            if (it->name.compare(opa.name) == 0) {
+                mod = &*it;
+                break;
+            }
+        }
+        if (fix.compare("postfix") == 0) {
+            ret = *mod;
+        }
+        if (uoperator.compare("++") == 0)  mod->value++;
+        if (uoperator.compare("--") == 0)  mod->value++;
+        if (fix.compare("prefix") == 0) {
+            ret = *mod;
+        }
+        return ret;
+    }
+
+    if (node->astType.compare("BinaryOperator") == 0) {
+        std::regex_search(node->text, smBinaryOperator, eBinaryOperator);
+        std::string castTo = smBinaryOperator[1];
+        std::string boperator = smBinaryOperator[2];
+        Variable opa, opb;
+        Variable ret;
+        opa = visit(node->child);
+        opb = visit(node->child->nextSib);
+        if (boperator.compare("<") == 0) {
+            ret.value = opa.value < opb.value;
+            return ret;
+        }
+        if (boperator.compare("=") == 0) {
+            Variable* mod = NULL;
+            for (auto it = vVariable.begin(); it != vVariable.end(); it++) {
+                if (it->name.compare(opa.name) == 0) {
+                    mod = &*it;
+                    break;
+                }
+            }
+            if (mod) mod->value = opb.value;
+            else mod = new Variable();
+            return *mod;
+        }
+        return ret;
+    }
     if (node->astType.compare("IfStmt") == 0) {
+        Variable cond, vtrue, vfalse;
+        cond = visit(node->child->nextSib->nextSib);
+        if (cond.value) {
+            vtrue = visit(node->child->nextSib->nextSib->nextSib);
+        }
+        else {
+            vfalse = visit(node->child->nextSib->nextSib->nextSib->nextSib);
+        }
+        return Variable();
+    }
+
+    if (node->astType.compare("ForStmt") == 0) {
+        Node *init, *cond, *iter, *stmt;
+        Variable vinit, vcond, viter, vstmt;
+        init = node->child;
+        cond = node->child->nextSib->nextSib;
+        iter = node->child->nextSib->nextSib->nextSib;
+        stmt = node->child->nextSib->nextSib->nextSib->nextSib;
+        vinit = visit(init);
+        while ((vcond = visit(cond)).value) {
+            vstmt = visit(stmt);
+            viter = visit(iter);
+        }
+        return Variable();
     }
 
     if (node->astType.compare("CompoundStmt") == 0) {
+        Variable temp;
+        Variable ret;
+        for (auto next = node->child; next != NULL; next = next->nextSib) {
+            temp = visit(next);
+        }
+        return ret;
+    }
+
+    if (node->astType.compare("TranslationUnitDecl") == 0) {
+        Variable temp;
+        for (auto next = node->child; next != NULL; next = next->nextSib) {
+            temp = visit(next);
+        }
+        return Variable();
     }
 
     if (node->astType.compare("DeclStmt") == 0) {
+        Variable temp = visit(node->child);
+        return temp;
     }
 
     if (node->astType.compare("<<<NULL") == 0) {
-
+        return Variable();
     }
 
+    if (node->astType.compare("TypedefDecl") == 0) {
+        return Variable();
+    }
+    
     if (node->child) {
         Variable t;
         t = visit(node->child);
     }
+
     if (node->nextSib) {
         Variable t;
         t = visit(node->nextSib);
@@ -256,6 +351,9 @@ unsigned long long cast(std::string str, unsigned long long v)
 {
     if (str.compare("int") == 0) {
         return (int)v;
+    }
+    if (str.compare("unsigned int") == 0) {
+        return (unsigned int)v;
     }
 
     std::cout << "Exception encountered\n";
